97de115487386ab798a92778510fef9d40edf735
AmalFaridunovich <amalbahrankulov@gmail.com>
2021-08-19 16:53:38 +0500

    Initial commit.

---
+++ Дополнительный/js1hw.js
@@ -0,0 +1,16 @@
+
+
+// 1.Введите в консоль слово Volvo из массива cars.
+let cars = ["Saab", "Volvo", "BMW"];
+
+console.log(cars[1]);
+
+
+// 2.Поменяте первое слово на Форд 
+const cars1 = ["Volvo", "Jeep", "Mercedes"];
+
+console.log(cars1[0] = "Ford" , cars1[1] , cars1[2]);
+
+// 3. Введите в консоль количество элементов массива
+const cars2 = ["Volvo", "Jeep", "Mercedes"];
+console.log(cars2.length);
---
+++ Основное/js1hw.js
@@ -0,0 +1,11 @@
+let styles = [ 'Jazz' , 'Blues' ]
+styles.push('Rock & Roll')
+styles[1] = 'Classic'
+console.log(styles);
+
+console.log(styles.shift());
+
+styles.unshift('Rap')
+styles.unshift('Reggi')
+
+console.log(styles);
